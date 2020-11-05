376790be3173d69270b242e2cbc16fa2c964282e
Mane <maneyeganyan@yahoo.com>
2020-11-05 23:16:59 +0400

    homework21

--- homework21/homework21.py
+++ homework21/homework21.py
@@ -2,19 +2,30 @@ import json
 import requests
 import threading
 
-
+new_list = []
 with open("json_photo.json", "r") as file:
 	file_1 = json.load(file)
-	file_2 = requests.get(file_1)
+
+	for i in file_1.values():
+		for m in i:
+			for k,s in m.items():
+				if k == "url":
+					new_list.append(s)
+
+
+	
+for j in new_list:
+	file_2 = requests.get(j)	
+
 
 def loading_function():
-	for i in file_1(items):
-		if i[-3:] =="jpg":
-			with open("jpgfile.jpg", "wb") as gpegfile:
-				jpgfile.write(i.content)
-		if i[-3:] =="png":
+	for gg in file_2:
+		if gg[-3:] =="jpg":
+			with open("jpgfile.jpg", "wb") as jpgfile:
+				jpgfile.write(gg.content)
+		if gg[-3:] =="png":
 			with open("pngfile.png", "wb") as pngfile:
-				pngfile.write(i.content)
+				pngfile.write(gg.content)
 
 
 if __name__ == "__main__":
